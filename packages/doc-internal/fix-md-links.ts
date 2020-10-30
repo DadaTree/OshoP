179e446fb6ecb4b06192757d8126a25e15f21faf
DadaTree <107853124+DadaTree@users.noreply.github.com>
2020-10-30 22:37:35 +0000

    Clean up some code

--- packages/doc-internal/fix-md-links.ts
+++ packages/doc-internal/fix-md-links.ts
@@ -1,3 +1,7 @@
 import { readFileSync, writeFileSync } from "fs";
 import glob from "glob";
-import { join }
+import { join } from "path";
+
+for (const mdFile of await glob("**/*.md", { cwd: "../../docs" })) {
+	console.log(mdFile);
+	const content = readFileSync(join("../
