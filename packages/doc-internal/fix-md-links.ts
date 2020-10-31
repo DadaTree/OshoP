e05b6fe53b35ef8528ccc2a33ed7abc9743f183d
DadaTree <107853124+DadaTree@users.noreply.github.com>
2020-10-31 11:22:40 +0000

    Upgrade jQuery

--- packages/doc-internal/fix-md-links.ts
+++ packages/doc-internal/fix-md-links.ts
@@ -4,4 +4,9 @@ import { join } from "path";
 
 for (const mdFile of await glob("**/*.md", { cwd: "../../docs" })) {
 	console.log(mdFile);
-	const content = readFileSync(join("../
+	const content = readFileSync(join("../../docs", mdFile)).toString();
+	writeFileSync(
+		join("../../docs", mdFile),
+		content
+			// Fix MD links
+			.replaceAll(/\([^)]+\.md\b(#[^)]+)?\)/g, (val
