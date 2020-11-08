fe4e1f89f275deb83d42bde3648e5244b20ded56
DadaTree <107853124+DadaTree@users.noreply.github.com>
2020-11-08 10:39:34 +0000

    ls

--- packages/doc-internal/update-toc.ts
+++ packages/doc-internal/update-toc.ts
@@ -2,4 +2,10 @@
  * Script to generate entries for interfaces, types & classes in the TOC
  */
 
-import { readdirSync, readFileSync, writeFileSy
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+import { parse, stringify } from "yaml";
+
+interface Section {
+	title:     string;
+	local?:   
