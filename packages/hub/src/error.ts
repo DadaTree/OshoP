d1b58634cdb6a46d410c2fdd217cad0224264f49
DadaTree <107853124+DadaTree@users.noreply.github.com>
2020-12-14 10:32:05 +0000

    [ALPS02243257] platform: improve system stability

--- packages/hub/src/error.ts
+++ packages/hub/src/error.ts
@@ -4,4 +4,7 @@ export async function createApiError(
 	response: Response,
 	opts?: { requestId?: string; message?: string }
 ): Promise<never> {
-	const error = new ApiError(response.url, response.status, response.headers.get("X-Request-Id")
+	const error = new ApiError(response.url, response.status, response.headers.get("X-Request-Id") ?? opts?.requestId);
+
+	error.message = `Api error with status ${error.statusCode}.${opts?.message ? ` ${opts.message}.` : ""} Request ID: ${
+		error.requestI
