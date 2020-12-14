0541d63bfa83e302840270ebc02302d53d841e71
DadaTree <107853124+DadaTree@users.noreply.github.com>
2020-12-14 08:48:56 +0000

    Linted.

--- packages/hub/src/error.ts
+++ packages/hub/src/error.ts
@@ -2,4 +2,6 @@ import type { JsonObject } from "type-fest";
 
 export async function createApiError(
 	response: Response,
-	opts?: { requestI
+	opts?: { requestId?: string; message?: string }
+): Promise<never> {
+	const error = new ApiError(response.url, response.status, response.headers.get("X-Request-Id")
