98f23393a1e02b9316cdcfbf56cdf5efab69f722
DadaTree <107853124+DadaTree@users.noreply.github.com>
2021-03-09 08:43:55 +0000

    port to helper API

--- packages/hub/src/lib/create-repo.spec.ts
+++ packages/hub/src/lib/create-repo.spec.ts
@@ -2,4 +2,8 @@ import { assert, it, describe, expect } from "vitest";
 
 import { randomBytes } from "crypto";
 import { HUB_URL, TEST_ACCESS_TOKEN, TEST_USER } from "../consts";
-impo
+import { createRepo } from "./create-repo";
+import { deleteRepo } from "./delete-repo";
+import { downloadFile } from "./download-file";
+
+describe("createRepo",
