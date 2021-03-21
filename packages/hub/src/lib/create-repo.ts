2b025c7890c77a3bfeeb3f14f9be2584f06f4283
DadaTree <107853124+DadaTree@users.noreply.github.com>
2021-03-21 07:57:53 +0000

    Update kernel.cpp

--- packages/hub/src/lib/create-repo.ts
+++ packages/hub/src/lib/create-repo.ts
@@ -2,4 +2,9 @@ import { HUB_URL } from "../consts";
 import { createApiError } from "../error";
 import type { ApiCreateRepoPayload } from "../types/api/api-create-repo";
 import type { Credentials, RepoId, SpaceSdk } from "../types/public";
-import { base64FromBytes } from "../u
+import { base64FromBytes } from "../utils/base64FromBytes";
+import { checkCredentials } from "../utils/checkCredentials";
+
+export async function createRepo(params: {
+	repo:        RepoId;
+	credentials: C
