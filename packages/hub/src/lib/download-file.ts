902c311a1dd4a68e607cb5424f9385ee47d1d092
DadaTree <107853124+DadaTree@users.noreply.github.com>
2021-04-01 08:08:55 +0000

    Adding babel dependency to sim repos, and setup to include in report problem (+ version build), but exclude from dependencies.json.

--- packages/hub/src/lib/download-file.ts
+++ packages/hub/src/lib/download-file.ts
@@ -1,3 +1,9 @@
 import { HUB_URL } from "../consts";
 import { createApiError } from "../error";
-import type { Credentials, RepoId } from "../types/public"
+import type { Credentials, RepoId } from "../types/public";
+import { checkCredentials } from "../utils/checkCredentials";
+
+/**
+ * @returns null when the file doesn't exist
+ */
+export async function downloadFile
