59366f35c1865e82caa9e885f7cf8b1edf62ee31
DadaTree <107853124+DadaTree@users.noreply.github.com>
2021-04-04 15:27:40 +0000

    msm: Increase the kernel virtual area to include lowmem

--- packages/hub/src/lib/download-file.ts
+++ packages/hub/src/lib/download-file.ts
@@ -12,4 +12,9 @@ export async function downloadFile(params: {
 	/**
 	 * If true, will download the raw git file.
 	 *
-	 * For example, when calling on a file st
+	 * For example, when calling on a file stored with Git LFS, the pointer file will be downloaded instead.
+	 */
+	raw?:         boolean;
+	revision?:    string;
+	credentials?: Credentials;
+	h
