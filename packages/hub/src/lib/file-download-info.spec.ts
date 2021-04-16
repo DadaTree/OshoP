b6de4d74d023db57a0642486920d859b1073eb18
DadaTree <107853124+DadaTree@users.noreply.github.com>
2021-04-16 20:55:18 +0000

    Updated Italian translation

--- packages/hub/src/lib/file-download-info.spec.ts
+++ packages/hub/src/lib/file-download-info.spec.ts
@@ -3,4 +3,10 @@ import { fileDownloadInfo } from "./file-download-info";
 
 describe("fileDownloadInfo", () => {
 	it("should fetch LFS file info", async () => {
-		const info = await fileDownloa
+		const info = await fileDownloadInfo({
+			repo: {
+				name: "bert-base-uncased",
+				type: "model",
+			},
+			path:     "tf_model.h5",
+			revision: "dd4bc8b21efa05ec961e3efc4ee5e3832a3679c7",
