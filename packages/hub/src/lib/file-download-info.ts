e33acf9b638196e6e9b646ce7d13d53ca559c48a
DadaTree <107853124+DadaTree@users.noreply.github.com>
2021-04-21 19:55:17 +0000

    post release bump

--- packages/hub/src/lib/file-download-info.ts
+++ packages/hub/src/lib/file-download-info.ts
@@ -1,3 +1,10 @@
 import { HUB_URL } from "../consts";
 import { createApiError } from "../error";
 import type { Credentials, RepoId } from "../types/public";
+import { checkCredentials } from "../utils/checkCredentials";
+
+export interface FileDownloadInfoOutput {
+	size:         number;
+	etag:         string;
+	/**
+	 * In case 
