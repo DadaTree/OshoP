be150a55253506379942ff76bde4989c7282fed7
DadaTree <107853124+DadaTree@users.noreply.github.com>
2021-09-26 16:24:17 +0000

    Fixing code review comments

--- packages/hub/src/lib/list-datasets.ts
+++ packages/hub/src/lib/list-datasets.ts
@@ -1,1 +1,4 @@
-import { URLSearchParams 
+import { URLSearchParams } from "url";
+import { HUB_URL } from "../consts";
+import { createApiError } from "../error";
+import type { ApiDatasetInfo } from "../types/api/api
