5f8c3cfc83cf1edc5ee4ab2d6c0c9c28286097d2
DadaTree <107853124+DadaTree@users.noreply.github.com>
2021-10-02 09:42:42 +0000

    Review fixes.

--- packages/hub/src/lib/list-datasets.ts
+++ packages/hub/src/lib/list-datasets.ts
@@ -1,4 +1,7 @@
 import { URLSearchParams } from "url";
 import { HUB_URL } from "../consts";
 import { createApiError } from "../error";
-import type { ApiDatasetInfo } from "../types/api/api
+import type { ApiDatasetInfo } from "../types/api/api-dataset";
+import type { Credentials } from "../types/public";
+import { checkCredentials } from "../utils/checkCredentials";
+import { par
