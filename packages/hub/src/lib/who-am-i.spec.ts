58c9a91e325f7aeb67d7a5414791f58a430e7e3f
DadaTree <107853124+DadaTree@users.noreply.github.com>
2021-11-23 17:13:52 +0000

    delet

--- packages/hub/src/lib/who-am-i.spec.ts
+++ packages/hub/src/lib/who-am-i.spec.ts
@@ -2,4 +2,6 @@ import { assert, it, describe } from "vitest";
 import { TEST_ACCESS_TOKEN } from "../consts";
 import { whoAmI } from "./who-am-i";
 
-describe("whoAmI", (
+describe("whoAmI", () => {
+	it("should fetch identity info", async () => {
+		const info = await whoAmI({ credentials: { accessToken: TEST_ACCESS_T
