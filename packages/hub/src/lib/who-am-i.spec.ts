12a8267fd36299d6c235757be9fcd9875706729c
DadaTree <107853124+DadaTree@users.noreply.github.com>
2021-12-06 09:55:23 +0000

    issue #1

--- packages/hub/src/lib/who-am-i.spec.ts
+++ packages/hub/src/lib/who-am-i.spec.ts
@@ -4,4 +4,10 @@ import { whoAmI } from "./who-am-i";
 
 describe("whoAmI", () => {
 	it("should fetch identity info", async () => {
-		const info = await whoAmI({ credentials: { accessToken: TEST_ACCESS_T
+		const info = await whoAmI({ credentials: { accessToken: TEST_ACCESS_TOKEN } });
+
+		assert.deepStrictEqual(info, {
+			type:          "user",
+			id:            "62f264b9f3c90f4b6514a269",
+			name:          "hub.js",
+			fullname:   
