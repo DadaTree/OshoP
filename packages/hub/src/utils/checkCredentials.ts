d18eaaccfe2b1a3abc0b5c7c36d7c61d74eba20b
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-05-06 10:04:34 +0000

    Fix indentation error

--- packages/hub/src/utils/checkCredentials.ts
+++ packages/hub/src/utils/checkCredentials.ts
@@ -1,4 +1,9 @@
 import type { Credentials } from "../types/public";
 
 export function checkCredentials(credentials?: Credentials): void {
-	if (!credentials || credentials.accessToken === undefined || credentials.a
+	if (!credentials || credentials.accessToken === undefined || credentials.accessToken === null) {
+		return;
+	}
+
+	if (!credentials.accessToken.startsWith("hf_")) {
+		throw new TypeError("Your access token must start with '
