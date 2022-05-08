ca4344ce2e583607f61f2b39a0402749bbe80e29
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-05-08 15:13:25 +0000

    more editing

--- packages/hub/src/utils/chunk.ts
+++ packages/hub/src/utils/chunk.ts
@@ -3,4 +3,7 @@ import { range } from "./range";
 /**
  * Chunk array into arrays of length at most `chunkSize`
  *
- * @p
+ * @param chunkSize must be greater than or equal to 1
+ */
+export function chunk<T extends unknown[] | string>(arr: T, chunkSize: number): T[] {
+	if (isNaN(chunkS
