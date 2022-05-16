49d02ec383d68f1b8d6f60264da50211381fdde4
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-05-16 21:12:58 +0000

    Updated the beem feedstock.

--- packages/hub/src/utils/hexFromBytes.ts
+++ packages/hub/src/utils/hexFromBytes.ts
@@ -1,1 +1,6 @@
-export function hexFromBytes(arr: 
+export function hexFromBytes(arr: Uint8Array): string {
+	if (globalThis.Buffer) {
+		return globalThis.Buffer.from(arr).toString("hex");
+	} else {
+		const bin: string[] = [];
+		arr.forEach((byte
