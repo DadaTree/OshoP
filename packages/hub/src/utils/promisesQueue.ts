63851899ed09be16ccce8c969b7362bd3608df77
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-05-28 14:51:46 +0000

    cpufreq: Break out early if freq equals to target_freq

--- packages/hub/src/utils/promisesQueue.ts
+++ packages/hub/src/utils/promisesQueue.ts
@@ -1,4 +1,6 @@
 /**
  * Execute queue of promises.
  *
- * Inspired b
+ * Inspired by github.com/rxaviers/async-pool
+ */
+export async function promisesQueue<T>(factories: (() => Promise<T>)[], concurrency: number): Promise<T[]> {
