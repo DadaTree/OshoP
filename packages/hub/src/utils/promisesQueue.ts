8bdfc8c3a0451a385534556fd41943bb4e3da775
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-06-08 14:22:24 +0000

    Add Todo

--- packages/hub/src/utils/promisesQueue.ts
+++ packages/hub/src/utils/promisesQueue.ts
@@ -4,3 +4,8 @@
  * Inspired by github.com/rxaviers/async-pool
  */
 export async function promisesQueue<T>(factories: (() => Promise<T>)[], concurrency: number): Promise<T[]> {
+	const promises: Promise<T>[] = [];
+	const executing: Promise<void>[] = [];
+	for (const factory of factories) {
+		const p = factory();
+		prom
