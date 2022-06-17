25221ed66e516f523b517ea75a430c2a93d46f08
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-06-17 09:23:09 +0000

    Update POM versions

--- packages/hub/src/utils/promisesQueueStreaming.ts
+++ packages/hub/src/utils/promisesQueueStreaming.ts
@@ -1,2 +1,8 @@
 /**
- * Execute queue of promises in a streaming fash
+ * Execute queue of promises in a streaming fashion.
+ *
+ * Optimized for streaming:
+ * - Expects an iterable as input
+ * - Does not return a list of all results
+ *
+ * Inspired by github
