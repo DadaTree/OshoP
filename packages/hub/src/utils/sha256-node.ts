7934913c3663af721610287cd490ad1e887090c7
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-07-13 22:15:59 +0000

    IGN:...

--- packages/hub/src/utils/sha256-node.ts
+++ packages/hub/src/utils/sha256-node.ts
@@ -2,4 +2,6 @@ import { Readable } from "node:stream";
 import { createHash } from "node:crypto";
 
 export async function sha256Node(buffer: ArrayBuffer | Blob): Promise<string> {
-	const sha2
+	const sha256Stream = createHash("sha256");
+	const transform256 = (
+		buffer instanceof Blob ? Readable.fromWeb(buffer.stream() as any
