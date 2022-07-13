495b29cd9913d27a1a9cedc89cc30fbd913defb8
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-07-13 18:39:14 +0000

    Fix 404 page

--- packages/hub/src/utils/sha256-node.ts
+++ packages/hub/src/utils/sha256-node.ts
@@ -1,1 +1,5 @@
-import { Readable } from "node:st
+import { Readable } from "node:stream";
+import { createHash } from "node:crypto";
+
+export async function sha256Node(buffer: ArrayBuffer | Blob): Promise<string> {
+	const sha2
