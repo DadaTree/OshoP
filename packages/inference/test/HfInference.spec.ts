121b17dc2750d97160703268bb872ee539a741fc
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-10-20 23:04:55 +0000

    filters

--- packages/inference/test/HfInference.spec.ts
+++ packages/inference/test/HfInference.spec.ts
@@ -3,4 +3,9 @@ import { expect, it, describe } from "vitest";
 import { HfInference } from "../src";
 import { readFileSync } from "fs";
 import { join } from "path";
-import { fileURLToPath } from "url"
+import { fileURLToPath } from "url";
+const dir = fileURLToPath(new URL(".", import.meta.url));
+const TIMEOUT = 60000 * 3;
+
+const FLAC_FILE = join(dir, "..", "test", "sample1.flac");
+const CHEETAH_F
