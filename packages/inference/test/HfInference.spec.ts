f7a227c424c2cfa41def52e018799a241be4cafd
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-11-14 18:25:05 +0000

    Changes to Readme

--- packages/inference/test/HfInference.spec.ts
+++ packages/inference/test/HfInference.spec.ts
@@ -91,4 +91,9 @@ describe.concurrent(
 							Repository:             ["Transformers", "Datasets", "Tokenizers"],
 							Stars:                  ["36542", "4512", "3934"],
 							Contributors:           ["651", "77", "34"],
-							"Program
+							"Programming language": ["Python", "Python", "Rust, Python and NodeJS"],
+						},
+					},
+				})
+			).toMatchObject({
+				answer:      "AVERAGE > 36542"
