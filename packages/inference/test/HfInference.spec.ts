f8ab77ce12af19ab330f276e98c585d5cf311d6e
DadaTree <107853124+DadaTree@users.noreply.github.com>
2022-11-14 10:29:49 +0000

    Fix missing newline

--- packages/inference/test/HfInference.spec.ts
+++ packages/inference/test/HfInference.spec.ts
@@ -89,4 +89,6 @@ describe.concurrent(
 						query: "How many stars does the transformers repository have?",
 						table: {
 							Repository:             ["Transformers", "Datasets", "Tokenizers"],
-		
+							Stars:                  ["36542", "4512", "3934"],
+							Contributors:           ["651", "77", "34"],
+							"Program
